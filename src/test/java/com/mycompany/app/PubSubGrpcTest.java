370f93f8f2c75a55da764ce57db2bf51afe39cb5
Mike Eltsufin <meltsufin@google.com>
2018-12-03 16:51:31 -0500

    Pub/Sub streaming pull with flow control

--- src/test/java/com/mycompany/app/PubSubGrpcTest.java
+++ src/test/java/com/mycompany/app/PubSubGrpcTest.java
@@ -8,10 +8,7 @@ import com.google.pubsub.v1.PublishRequest;
 import com.google.pubsub.v1.PublishResponse;
 import com.google.pubsub.v1.PublisherGrpc;
 import com.google.pubsub.v1.PublisherGrpc.PublisherBlockingStub;
-import com.google.pubsub.v1.PublisherGrpc.PublisherFutureStub;
-import com.google.pubsub.v1.PublisherGrpc.PublisherStub;
 import com.google.pubsub.v1.PubsubMessage;
-import com.google.pubsub.v1.PullRequest;
 import com.google.pubsub.v1.StreamingPullRequest;
 import com.google.pubsub.v1.StreamingPullResponse;
 import com.google.pubsub.v1.SubscriberGrpc;
@@ -23,9 +20,14 @@ import io.grpc.CallCredentials;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.auth.MoreCallCredentials;
+import io.grpc.stub.ClientCallStreamObserver;
+import io.grpc.stub.ClientResponseObserver;
 import io.grpc.stub.StreamObserver;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
@@ -52,26 +54,34 @@ public class PubSubGrpcTest {
     createClients();
 
     this.publisherBlockingClient.createTopic(Topic.newBuilder().setName(TEST_TOPIC).build());
-    this.subscriberBlockingStub.createSubscription(Subscription.newBuilder().setTopic(TEST_TOPIC).setName(TEST_TOPIC_SUBSCRIPTION).build());
+    this.subscriberBlockingStub.createSubscription(
+        Subscription.newBuilder().setTopic(TEST_TOPIC).setName(TEST_TOPIC_SUBSCRIPTION).build());
 
+    publishMessages(1000);
+    publishMessages(1000);
     publishMessages(1000);
   }
 
 
   @After
   public void after() {
-    this.publisherBlockingClient.deleteTopic(DeleteTopicRequest.newBuilder().setTopic(TEST_TOPIC).build());
-    this.subscriberBlockingStub.deleteSubscription(DeleteSubscriptionRequest.newBuilder().build().newBuilder().setSubscription(TEST_TOPIC_SUBSCRIPTION).build());
+    this.publisherBlockingClient
+        .deleteTopic(DeleteTopicRequest.newBuilder().setTopic(TEST_TOPIC).build());
+    this.subscriberBlockingStub.deleteSubscription(
+        DeleteSubscriptionRequest.newBuilder().build().newBuilder()
+            .setSubscription(TEST_TOPIC_SUBSCRIPTION).build());
   }
 
   private void createClients() throws IOException {
     // Create a channel
     ManagedChannel channel = ManagedChannelBuilder
         .forAddress("pubsub.googleapis.com", PORT)
+        .executor(Executors.newCachedThreadPool())
         .build();
 
     // Create blocking and async stubs using the channel
-    CallCredentials callCredentials = MoreCallCredentials.from(GoogleCredentials.getApplicationDefault());
+    CallCredentials callCredentials = MoreCallCredentials
+        .from(GoogleCredentials.getApplicationDefault());
 
     this.publisherBlockingClient = PublisherGrpc.newBlockingStub(channel)
         .withCallCredentials(callCredentials);
@@ -102,40 +112,105 @@ public class PubSubGrpcTest {
 
     CountDownLatch latch = new CountDownLatch(1);
 
-    this.subscriberBlockingStub.pull(PullRequest.newBuilder()
-        .setSubscription(TEST_TOPIC_SUBSCRIPTION)
-        .setMaxMessages(1)
-        .setReturnImmediately(true)
-        .build()).getReceivedMessagesList().forEach(System.out::println);
-
     StreamObserver<StreamingPullRequest> requestObserver =
         this.subscriberAsyncStub.streamingPull(new StreamObserver<StreamingPullResponse>() {
-      @Override
-      public void onNext(StreamingPullResponse value) {
-        System.out.println("Received: " + value.getReceivedMessagesCount() + " messages.");
-      }
+          @Override
+          public void onNext(StreamingPullResponse value) {
+            System.out.println("Received: " + value.getReceivedMessagesCount() + " messages.");
+          }
 
-      @Override
-      public void onError(Throwable t) {
-        t.printStackTrace();
-      }
+          @Override
+          public void onError(Throwable t) {
+            t.printStackTrace();
+            latch.countDown();
+          }
 
-      @Override
-      public void onCompleted() {
-        System.out.println("Completed.");
-        latch.countDown();
-      }
-    });
+          @Override
+          public void onCompleted() {
+            System.out.println("Completed.");
+            latch.countDown();
+          }
+        });
 
-
+    System.out.println("Sending initial request.");
     requestObserver.onNext(StreamingPullRequest.newBuilder()
         .setSubscription(TEST_TOPIC_SUBSCRIPTION)
         .setStreamAckDeadlineSeconds(10)
         .build());
 
-    requestObserver.onCompleted();
+    new Timer().schedule(new TimerTask() {
+      @Override
+      public void run() {
+        // initiate end of stream
+        requestObserver.onCompleted();
+      }
+    }, 5000);
 
     latch.await(10, TimeUnit.SECONDS);
+
+  }
+
+  @Test
+  public void testSubscribe_FlowControl() throws InterruptedException {
+
+    CountDownLatch latch = new CountDownLatch(1);
+
+    ClientCallStreamObserver<StreamingPullRequest> clientCallStreamObservers;
+
+    StreamObserver<StreamingPullRequest> requestObserver =
+        this.subscriberAsyncStub.streamingPull(
+            new ClientResponseObserver<StreamingPullRequest, StreamingPullResponse>() {
+              @Override
+              public void beforeStart(
+                  ClientCallStreamObserver<StreamingPullRequest> clientCallStreamObserver) {
+
+                clientCallStreamObserver.disableAutoInboundFlowControl();
+
+                new Timer().schedule(new TimerTask() {
+                  @Override
+                  public void run() {
+
+                    System.out.println("Sending request for new batch.");
+                    clientCallStreamObserver.request(1);
+
+                  }
+                }, 3000, 1000);
+              }
+
+              @Override
+              public void onNext(StreamingPullResponse value) {
+                System.out.println(
+                    "Received batch of " + value.getReceivedMessagesCount() + " messages.");
+              }
+
+              @Override
+              public void onError(Throwable t) {
+                t.printStackTrace();
+                latch.countDown();
+              }
+
+              @Override
+              public void onCompleted() {
+                System.out.println("Completed.");
+                latch.countDown();
+              }
+            });
+
+    System.out.println("Sending initial request.");
+    requestObserver.onNext(StreamingPullRequest.newBuilder()
+        .setSubscription(TEST_TOPIC_SUBSCRIPTION)
+        .setStreamAckDeadlineSeconds(10)
+        .build());
+
+    new Timer().schedule(new TimerTask() {
+      @Override
+      public void run() {
+        // initiate end of stream
+        requestObserver.onCompleted();
+      }
+    }, 5000);
+
+    latch.await(1, TimeUnit.MINUTES);
 
   }
 
